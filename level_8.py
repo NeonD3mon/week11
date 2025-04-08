33740a82ed04e858ebcaaeaa01d89f2f5d745b79
Itamar Afek <ItamarAfek@Itamars-Mac.local>
2025-04-08 19:55:31 -0400

    level 8 complete

---
+++ level_8.py
@@ -0,0 +1,4 @@
+input() # When no prompt is given, the computer will still wait for an answer from the user, without any massage.
+
+Im_a_variable = 5 == 5
+input(Im_a_variable) # You can definately use variables as inputs. The program use that as a string for it's prompt
