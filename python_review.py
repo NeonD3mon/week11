b5ece7c848a509a118deb10f27f0ebca2791cf08
Itamar Afek <ItamarAfek@Itamars-Mac.local>
2025-04-08 19:02:37 -0400

    Level 4 complete

--- python_review.py
+++ python_review.py
@@ -2,3 +2,14 @@
 print("It Worked!")
 
 x = None
+
+
+a = 3 # The variable called "a" is now holding the integer value of 3
+b = 4.0 # The variable called "b" is now holding the float value of 4
+c = True # The variable called "c" is now holding the boolean value of True
+d = False # The variable called "d" is now holding the boolean value of False
+e = "Hey!" # The variable called "e" is now holding the string value of "Hey!"
+f = # The variable called "f" is now holding no value
+age = 32 # he variable called "age" is now holding the integer value of 32
+Name = "Itamar" # The variable called "Name" is now holding the string value of "Itamar"
+Instrument = "Piano" # The variable called "Instrument" is now holding the string value of "Piano"
