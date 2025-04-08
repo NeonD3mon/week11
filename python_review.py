abc677214d2faaa4463ed706995a757af811df67
Itamar Afek <ItamarAfek@Itamars-Mac.local>
2025-04-08 19:24:51 -0400

    Level 7 complete

--- python_review.py
+++ python_review.py
@@ -9,7 +9,7 @@ b = 4.0 # The variable called "b" is now holding the float value of 4
 c = True # The variable called "c" is now holding the boolean value of True
 d = False # The variable called "d" is now holding the boolean value of False
 e = "Hey!" # The variable called "e" is now holding the string value of "Hey!"
-f = # The variable called "f" is now holding no value
+f = None # The variable called "f" is now holding the value of none
 age = 32 # he variable called "age" is now holding the integer value of 32
 Name = "Itamar" # The variable called "Name" is now holding the string value of "Itamar"
 Instrument = "Piano" # The variable called "Instrument" is now holding the string value of "Piano"
@@ -25,6 +25,50 @@ $$$Variabel = "bad"
 
 '''
 
-x = type(3)
-y = type("Hello")
-z = type(True)
+x = type(3) # This is an integer
+y = type("Hello") # This is a string
+z = type(True) # This is a boolean
+
+print(x)
+print(y)
+print(z)
+
+my_integer = 42 # integer
+my_integer_type = type(my_integer)
+print(my_integer_type)
+
+my_float = 3.14 # float
+my_float_type = type(my_float)
+print(my_float_type)
+
+my_string = "Hello, World" # string
+my_string_type = type(my_string)
+print(my_string_type)
+
+my_boolean_true = True # boolean
+my_boolean_true_type = type(my_boolean_true)
+print(my_boolean_true_type)
+
+my_boolean_false = False # boolean
+my_boolean_false_type = type(my_boolean_false)
+print(my_boolean_false_type)
+
+my_none_value = None # Nonetype
+my_none_value_type = type(my_none_value)
+print(my_none_value_type)
+
+my_negative_integer = -7 # integer
+my_negative_integer_type = type(my_negative_integer)
+print(my_negative_integer_type)
+
+my_large_float = 1.23456789e10 # float
+my_large_float_type = type(my_large_float)
+print(my_large_float_type)
+
+my_single_character = 'A' # string
+my_single_character_type = type(my_single_character)
+print(my_single_character_type)
+
+my_empty_string = "" # string
+my_empty_string_type = type(my_empty_string)
+print(my_empty_string_type)
