6acd521b133104d895107cb68c26ebc04a39092c
Itamar Afek <ItamarAfek@Itamars-Mac.local>
2025-04-08 20:21:07 -0400

    Level 9 complete

---
+++ Number_Comparison.py
@@ -0,0 +1,11 @@
+Number1 = input("What is the 1st number?")
+Number2 = input("What is the 2nd number?")
+
+
+if (Number1 > Number2):
+    print(Number1, " is bigger than", Number2)
+elif (Number1 == Number2):
+    print(Number1, "=", Number2)
+else:
+    print(Number2, " is bigger than", Number1)
+
